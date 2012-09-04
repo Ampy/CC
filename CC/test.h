a87c44f22b271ac799af93f460875d41b5ef5533
ampy <ampy.apple@gmail.com>
2012-09-04 17:37:19 +0800

    1

---
+++ CC/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  CC
+//
+//  Created by  rtsafe02 on 12-9-4.
+//  Copyright (c) 2012年 TelSafe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ CC/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  CC
+//
+//  Created by  rtsafe02 on 12-9-4.
+//  Copyright (c) 2012年 TelSafe. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
