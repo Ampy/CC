81a41ecfc60e53a0444a3e9c9bae2e159162b0a0
ampy <ampy.apple@gmail.com>
2012-09-04 17:24:17 +0800

    Initial Commit

---
+++ CC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CC
+//
+//  Created by  rtsafe02 on 12-9-4.
+//  Copyright (c) 2012年 TelSafe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
