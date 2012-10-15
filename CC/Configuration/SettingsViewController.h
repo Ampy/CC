bcb77d0ac38f889dbcbf29436db009e7a0bcdc92
ampy <ampy.apple@gmail.com>
2012-10-15 16:23:53 +0800

    mengp

---
+++ CC/Configuration/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  CC
+//
+//  Created by  rtsafe02 on 12-10-15.
+//  Copyright (c) 2012年 TelSafe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ CC/Configuration/SettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  SettingsViewController.m
+//  CC
+//
+//  Created by  rtsafe02 on 12-10-15.
+//  Copyright (c) 2012年 TelSafe. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+@end
+
+@implementation SettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
