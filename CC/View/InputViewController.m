fb6cfc7308c6b0c2d57a41fda50b878cea7e1952
ampy <ampy.apple@gmail.com>
2012-10-16 16:39:25 +0800

    mengp

--- CC/View/InputViewController.m
+++ CC/View/InputViewController.m
@@ -137,14 +137,17 @@
         dispatch_async(dispatch_get_main_queue(), ^{
             if([Common ExceptionHandler:[LogicBase BuildCheckData]])
                 return;
+            [Config SetPlistInfo:@"CreateTable" Value:@"T"];
         });
         
         dispatch_async(dispatch_get_main_queue(),^{
+            [Config SetPlistInfo:@"popviewbegin" Value:@"T"];
             MaskView.hidden=true;
             WaitWebView.hidden=true;
             NSString * mid = [Config GetPlistInfo:@"InspectActivityID"];
             InspectViewController * inspectview = [[InspectViewController alloc] initWithInspectActivityId:mid];
             [self.navigationController pushViewController:inspectview animated:YES];
+            [Config SetPlistInfo:@"popviewend" Value:@"T"];
             //int i=[SecondItemTableView.visibleCells count];
             //MaskWebView.hidden=true;
             //MaskWebView setNeedsDisplay];
