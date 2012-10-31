f6da6dd4eb4af64abbab08fe566ebf2388f6f19b
ampy <ampy.apple@gmail.com>
2012-10-31 13:51:37 +0800

    mengp

--- CC/Services/Common.m
+++ CC/Services/Common.m
@@ -45,9 +45,12 @@
 
 +(int) CheckNetworkStatus
 {
-    Reachability *r = [Reachability reachabilityWithHostname:[Settings ServiceUrl ]];
+  
+    Reachability *r = [Reachability reachabilityWithHostname:[Settings ServiceUrl]];
+
     if ([r currentReachabilityStatus]==NotReachable)
     {
+        NSLog(@"%@ 网络无法到达",[Settings ServiceUrl]);
         return 21;
     }
     else 
