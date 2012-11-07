7adc2860e22cf43e3efb9300459d82ac75c7723a
ren fy <renfy@renmatoMacBook-Pro.local>
2012-11-07 10:15:42 +0800

    rfy

--- CC/Controllers/WelcomeViewController.m
+++ CC/Controllers/WelcomeViewController.m
@@ -49,14 +49,10 @@
 -(void) UpdateAndGoto
 {
     //[NSThread sleepForTimeInterval:1];
-    
-    //if(![Common ExceptionHandler:[Common CheckNetworkStatus]])
-    //{
-//    NSLog(@"%d",[Common networkState]);
 //    if(![Common ExceptionHandler:[Common networkState]])
 //    {
         [Common ExceptionHandler:[LogicBase UpdateByService]];
-    //}
+
 //    }
     LoginViewController *ctrl = [[LoginViewController alloc] init];
     [self.navigationController pushViewController:ctrl animated:YES];
